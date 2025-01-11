00af054d03c04b16d35d5e1e94b63a14543723b8
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-01-11 12:13:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let hello=prompt('String typedagi malumotni kiriting');
+let world=prompt('Number typedagi malumotni kiriting');
+let work=prompt('boolen typedagi malumotni kiriting');
+let zz='hamma malumotlarni togri kiritganingizga ishonchingiz komilmi';
+alert(`Siz kiritgan ${hello} sozi string typega tegishli\n Siz kiritgan ${world} sozi number typega tegishli\n Siz kiritgan ${work} sozi boolean typega tegishli`)
